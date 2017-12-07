e8f3a009ffc4d63af923da82799d7199000e2483
taihon <taihon@users.noreply.github.com>
2017-12-07 20:26:53 +0700

    send emails

--- SampleMVC/Services/EmailSender.cs
+++ SampleMVC/Services/EmailSender.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Threading.Tasks;
 
 namespace SampleMVC.Services
@@ -9,9 +11,17 @@ namespace SampleMVC.Services
     // For more details see https://go.microsoft.com/fwlink/?LinkID=532713
     public class EmailSender : IEmailSender
     {
-        public Task SendEmailAsync(string email, string subject, string message)
+        public async Task SendEmailAsync(string email, string subject, string message)
         {
-            return Task.CompletedTask;
+            var client = new SmtpClient("smtp.gmail.com", 587)
+            {
+                EnableSsl = true,
+                UseDefaultCredentials = false,
+                Credentials =
+                    new NetworkCredential("sample.mvc.2017@gmail.com", "4rfv$RFV")
+            };
+            var msg = new MailMessage("sample.mvc.2017@gmail.com", email, subject, message);
+            await client.SendMailAsync(msg);
         }
     }
 }
